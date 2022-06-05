689bf44ab82770ed83ee4f122c52c1b0001df274
Nishit Chittora <nishitchittora2015@gmail.com>
2022-06-05 21:53:12 +0530

    firebase config added

---
+++ firebase/clientApp.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_apiKey,
+  authDomain: process.env.NEXT_PUBLIC_authDomain,
+  projectId: process.env.NEXT_PUBLIC_projectId,
+  storageBucket: process.env.NEXT_PUBLIC_storageBucket,
+  messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
+  appId: process.env.NEXT_PUBLIC_appId,
+  measurementId: process.env.NEXT_PUBLIC_measurementId,
+};
+
+// Initialize Firebase
+if (!firebase.apps.length) firebase.initializeApp(firebaseConfig);
+
+export default firebase;
+
+// if (app.name && typeof window !== "undefined") {
+//   analytics = getAnalytics(app);
+// }
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,25 @@
 import Head from "next/head";
 import firebase from "../firebase/clientApp";
-import { getFirestore, collection } from "firebase/firestore";
-
 import { useCollection } from "react-firebase-hooks/firestore";
 
 export default function Home() {
+  const db = firebase.firestore();
   const [books, booksLoading, booksError] = useCollection(
-    collection(getFirestore(firebase), "books"),
-    {
-      snapshotListenOptions: { includeMetadataChanges: true },
-    }
+    db.collection("books"),
+    {}
   );
   console.log(books, "%%%", booksLoading);
   if (!booksLoading && books) {
     console.log(books.docs);
-    books.docs.map((doc) => console.log(doc));
+    books.docs.map((doc) => console.log(doc.data()));
   }
 
-  return <div></div>;
+  return (
+    <div>
+      hello
+      {books &&
+        !booksLoading &&
+        books.docs.map((doc) => <div>{doc.data().book}</div>)}
+    </div>
+  );
 }
